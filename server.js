d21f9de5348d1fdea77f23b893d8b6fd521937e6
Brandt Strom <Brandt Strom>
2016-02-06 22:48:26 -0800

    More server.js changes

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var morgan = require('morgan');
+var path = require('path');
 var express = require('express');
 app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'jade');
+app.locals.pretty = true; // pretty html
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 
 app.get('/', function(req, res) {
-    res.send("Hello world!");
+    res.render('index.jade');
 });
 
-app.listen(8080);
-console.log("Listening on port 8080");
+var port = process.env.PORT || 8080;
 
-
+app.listen(port);
+console.log("Listening on port " + port);
 
 module.exports = app;
