7ef067485c6e3f1ac0309005752621570cf1b28f
brandtkstrom <brandtkstrom@gmail.com>
2016-02-08 20:53:04 -0800

    Login/register validation and routing progress

--- routing/auth.js
+++ routing/auth.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+var User = require('../app/models/user');
 
 // Define routes used for authentication
-module.exports = function(app) {
+module.exports = function(app, passport) {
 
-    router.post('login', function(req, res) {
-        // TODO
-    });
+    router.route('/login')
+        .get(function(req, res) {
+            res.render('login');
+        })
+        .post(passport.authenticate('local', {
+                successRedirect: '/',
+                failureRedirect: 'login'
+        }));
     router.post('logout', function(req, res) {
         // TODO
     });
-    router.post('signup', function(req, res) {
-        // TODO
-    });
+    router.route('/register')
+        .get(function(req, res) {
+            res.render('register');
+        })
+        .post(function(req, res, next) {
+            var newUser = new User({ username: req.body.username });
+            User.register(newUser, req.body.password, function(err, user) {
+                if (err) {
+                    return res.render('register', { user : user });
+                }
+                    res.redirect('/login');
+            });
+        });
 
-    app.use('/auth', router);
+    app.use(router);
 };
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var morgan = require('morgan');
 var path = require('path');
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var express = require('express');
 app = express();
@@ -10,18 +16,27 @@ var router = express.Router();
 app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'jade');
 app.locals.pretty = true; // pretty html
-
+app.use(flash());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 
-// Setup routes
-require('./routing/auth.js')(app);
-require('./routing/routes.js')(app);
+// Passport config
+var User = require('./app/models/user');
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Setup database
 require('./config/db.js')(mongoose);
 
-var port = process.env.PORT || 8080;
+// Setup routes
+require('./routing/auth.js')(app, passport);
+require('./routing/routes.js')(app);
+
+var port = process.env.PORT || 1337;
 
 app.listen(port);
 console.log("Listening on port " + port);
