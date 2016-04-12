7c27b09f27e021e9a58c9468b4d4913dbb72dc93
youngbw <Athero23@gmail.com>
2016-04-11 21:01:16 -0700

    fixed the connection issue with mongoose for locally run mongodb instance

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 var args = require('yargs').argv;
+var mongoose = require('mongoose')
 
 module.exports = function(mongoose)
 {
     var SERVER_DB = 'mongodb://wolfpack253.ddns.net:27017/wolfpack';
-    var LOCAL_DB = 'mongodb://localhost:27017/wolfpack';
+    var LOCAL_DB = 'mongodb://localhost/wolfpack';
 
     var connection = (args.local || args.l) ? LOCAL_DB : SERVER_DB;
+
+    mongoose.connect(connection);
 
     mongoose.connection.on('error', function () {
         console.log("Mongoose - error connecting to db @ " + connection);
@@ -14,5 +17,5 @@ module.exports = function(mongoose)
         console.log("Mongoose connected to the database @ " + connection);
     });
 
-    mongoose.connect(SERVER_DB);
+
 }
