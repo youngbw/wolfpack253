f5fe04c2e14cb16dd1c2b8fcb7316ed859949578
Brent Young <youngbw@bitbucket.org>
2016-04-16 17:28:40 -0700

    events stuff

--- public/components/events/EventsController.js
+++ public/components/events/EventsController.js
@@ -2,4 +2,16 @@ angular.module('wolfpackApp').controller('EventsController', ['$scope', function
 
 
 	$scope.tagline = 'Events Page';
+
+	$scope.loading = false;
+
+
+	$scope.days = [
+		{date: 0}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1},
+		{date: 7}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1},
+		{date: 14}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1},
+		{date: 21}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}
+	];
+
+
 }]);
