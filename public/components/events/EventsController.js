21b3ee65a434e254de1f32eb5b25d40d9da674e4
youngbw <Athero23@gmail.com>
2016-04-17 02:54:01 -0700

    added some mock data to events calendar

--- public/components/events/EventsController.js
+++ public/components/events/EventsController.js
@@ -1,4 +1,4 @@
-angular.module('wolfpackApp').controller('EventsController', ['$scope', function($scope) {
+angular.module('wolfpackApp').controller('EventsController', function($scope) {
 
 
 	$scope.tagline = 'Events Page';
@@ -7,11 +7,11 @@ angular.module('wolfpackApp').controller('EventsController', ['$scope', function
 
 
 	$scope.days = [
-		{date: 0}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1},
+		{date: 0, events: [{name: 'Brents Birthday', note: 'Everyone is invited!'}, {name: '4th of July', note: 'National Holiday'}]}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1},
 		{date: 7}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1},
 		{date: 14}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1},
 		{date: 21}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}, {date: 1}
 	];
 
 
-}]);
+});
